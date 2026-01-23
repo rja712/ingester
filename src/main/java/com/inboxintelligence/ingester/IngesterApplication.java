406aefeeb2000e3bbdd6c200f2c38e16a1f3674d
Ankit Raj <rja.ankit@gmail.com>
2026-01-23 14:22:23 +0530

    init

---
+++ src/main/java/com/inboxintelligence/ingester/IngesterApplication.java
@@ -0,0 +1,13 @@
+package com.inboxintelligence.ingester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngesterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inboxintelligence/ingester/IngesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inboxintelligence.ingester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
